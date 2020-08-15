103b77ecf37f9c1085e5bf74a7113774a7db4db9
piyush <piyushkvagcr@gmail.com>
2020-08-15 11:18:15 +0530

    Updates

---
+++ precache-manifest.ed7a439c12e150824bea41156692bdb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91213e5f19ce0a6a2b621f37fe85f5c",
+    "url": "/gifpractice/index.html"
+  },
+  {
+    "revision": "359086c225e2ba5874f9",
+    "url": "/gifpractice/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "3098a89d4ae5c7dbcf92",
+    "url": "/gifpractice/static/css/main.3c04edf2.chunk.css"
+  },
+  {
+    "revision": "359086c225e2ba5874f9",
+    "url": "/gifpractice/static/js/2.40b8965a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gifpractice/static/js/2.40b8965a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3098a89d4ae5c7dbcf92",
+    "url": "/gifpractice/static/js/main.a5d82dd6.chunk.js"
+  },
+  {
+    "revision": "d33825cf9d6bcef1a940",
+    "url": "/gifpractice/static/js/runtime-main.2a85ce5f.js"
+  }
+]);
